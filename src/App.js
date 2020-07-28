6f0dd8095c65482d0622d95fc857007667da8109
HASSAN AMJAD <hassan004z@hotmail.com>
2020-07-28 22:26:10 +0500

    no message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-  
+const Square = require ('./src/Square')
+ 
 const App = () => {
     const onBtnClick = () => {
         alert('hello world');
     }
 
     return (
+      <React.Fragment>
+        <Square/>
         <button onClick={onBtnClick}>
         click me please!
         </button>
+        </React.Fragment>
     )
     }
 
