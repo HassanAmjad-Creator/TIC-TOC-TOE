1a821086ece066b0f3ce8d00fe2fba4c8147d171
HASSAN AMJAD <hassan004z@hotmail.com>
2020-07-31 22:00:09 +0500

    no message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const Square = require ('./Square');
+const React = require("react");
+const ReactDOM = require("react-dom");
+const Square = require("./Square");
 
 const App = () => {
-    const onBtnClick = () => {
-        alert('hello world');
-    }
+  const onBtnClick = () => {
+    alert("hello world");
+  };
 
-    return (
-      <React.Fragment>
-        <Square/>
-        <button onClick={onBtnClick}>
-        click me please!
-        </button>
-        </React.Fragment>
-    )
-    }
+  return (
+    <React.Fragment>
+      <Square />
+      <button onClick={onBtnClick}>click me please!</button>
+    </React.Fragment>
+  );
+};
 
-    if (typeof document === "undefined") {
-        module.exports = App 
-    } else {
-        ReactDOM.hydrate(<App/>,document.getElementById("root"));
-    }
-    
+if (typeof document === "undefined") {
+  module.exports = App;
+} else {
+  ReactDOM.hydrate(<App />, document.getElementById("root"));
+}
